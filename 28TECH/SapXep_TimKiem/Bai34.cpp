5e3bcbbfa8d1c0b6f514eedbfb138f3facdd49aa
chituong217 <chituong217@gmail.com>
2025-11-22 11:43:43 +0700

    Bai 34 sort search

---
+++ 28TECH/SapXep_TimKiem/Bai34.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main(){
+    int n, m, k;
+    cin >> n >> m >> k;
+    multiset<int> a, b;
+    for (int i = 0; i < n; i++){
+        int tmp; cin >> tmp;
+        a.insert(tmp);
+    }
+    for (int i = 0; i < m; i++){
+        int tmp; cin >> tmp;
+        b.insert(tmp);
+    }
+
+    auto it1 = a.begin();
+    auto it2 = b.begin();
+    int cnt = 0;
+    while (it1 != a.end() && it2 != b.end()){
+        if (*it1 - *it2 > k){
+            it2++;
+        }
+        else if (*it2 - *it1 > k){
+            it1++;
+        }
+        else{
+            cnt++;
+            it1++;
+            it2++;
+        }
+    }
+    cout << cnt << endl;
+    return 0;
+}
