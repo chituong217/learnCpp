20e76bf10ed993f7d2e4d2c50e0ef4078c378cba
chituong217 <chituong217@gmail.com>
2025-11-22 11:36:06 +0700

    Bai 33 sort search

---
+++ 28TECH/SapXep_TimKiem/Bai33.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    set <int> se;
+    for (int i = 0; i < n; i++){
+        int tmp; cin >> tmp;
+        se.insert(tmp);
+    }
+    cout << se.size();
+    return 0;
+}
