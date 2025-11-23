cad45031d0b95fa0a8877ac1e554c760660d1ff8
chituong217 <chituong217@gmail.com>
2025-11-23 10:54:46 +0700

    bai38

---
+++ 28TECH/SapXep_TimKiem/Bai38.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector <pair <int,int> > a;
+    for (int i = 0; i < n; i++){
+        int start, end;
+        cin >> start >> end;
+        a.push_back({start, 1});
+        a.push_back({end, -1});
+    }
+    sort(a.begin(), a.end());
+    int current_customers = 0;
+    int max_customers = 0;
+    for (int i = 0; i < (int)a.size(); i++){
+        current_customers += a[i].second;
+        if (current_customers > max_customers){
+            max_customers = current_customers;
+        }
+    }
+    cout << max_customers << endl;
+
+    return 0;
+}
