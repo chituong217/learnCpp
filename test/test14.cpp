95242d4adec6d1c3595d6eb3ac0532fb24fc44af
chituong217 <chituong217@gmail.com>
2025-11-25 22:44:54 +0700

    test14 update

--- test/test14.cpp
+++ test/test14.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 
 using namespace std;
 
 int main(){
-    string name1 = "Chi";
-    string name2 = "Tuong";
-    string name = "Nguyen Le " + name1 + " " + name2 + ' ' + "DEPTRAIVAIZZZZZZZZZZZZZZZ";
-    cout << name << endl;
+    string s;
+    getline(cin, s);
+    stringstream ss(s); // khoi tao ss = s
+    string word;
+    while (getline(ss, word, '/')){
+        cout << word << endl;
+    }
     return 0;
 }
