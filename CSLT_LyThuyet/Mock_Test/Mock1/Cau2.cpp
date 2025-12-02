63238a3ccdd4ad617c8b23322b5987e3de596a4f
chituong217 <chituong217@gmail.com>
2025-12-02 23:56:17 +0700

    cau 2

---
+++ CSLT_LyThuyet/Mock_Test/Mock1/Cau2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int findLongestAlternatingSubarray(int* nums, int size){
+    int longest = 0;
+    int i = 1;
+    while (i < size){
+        int currentLength = 1;
+        while (nums[i] * nums[i-1] < 0 && i < size){
+            i++;
+            currentLength++;
+        }
+        if (currentLength > longest){
+            longest = currentLength;
+        }
+    }
+    return longest;
+}
