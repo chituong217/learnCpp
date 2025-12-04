e4045291a5529b7131dfb8ab67ade272d638a6a1
chituong217 <chituong217@gmail.com>
2025-12-04 20:27:15 +0700

    cau 1

---
+++ CSLT_LyThuyet/Mock_Test/Mock3/Cau1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+void primeFactorization(int n){
+    for (int i = 2; i*i <= n; i++){
+        while (n % i == 0){
+            cout << i << " ";
+            n /= i;
+        }
+    }
+    if (n > 1) cout << n;
+    cout << endl;
+}
