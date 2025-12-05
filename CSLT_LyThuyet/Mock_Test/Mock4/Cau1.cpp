af1c9c22b3f95b71227bb47bb643f47f0229a6c2
chituong217 <chituong217@gmail.com>
2025-12-05 20:09:32 +0700

    cau 1

--- CSLT_LyThuyet/Mock_Test/Mock4/Cau1.cpp
+++ CSLT_LyThuyet/Mock_Test/Mock4/Cau1.cpp
@@ -3,31 +3,21 @@
 using namespace std;
 
 bool isMountainArray(int* a, int n){
-    bool daTang = false;
-    for (int i = 0; i < n; i++){
-        if (daTang == false){
-            if (i == 0 && a[i] >= a[i+1]) return false;
-            while (a[i] < a[i+1]){
-                i++;
-            }
-            if (a[i] > a[i+1]) daTang = true;
-            else if (a[i] == a[i+1]) return false;
-        }
-        else{
-            if (i == n - 1){
-                if (a[i] < a[i-1]){
-                    return true;
-                }
-                else{
-                    return false;
-                }
-            }
-            while (i + 1 < n && a[i] < a[i+1]){
-                i++;
-            }
-        }
+    if (n < 3) return false;
+
+    int i = 0;
+
+    while (i + 1 < n && a[i] < a[i + 1]){
+        i++;
     }
-    return false;
+
+    if (i == 0 || i == n - 1) return false;
+
+    while (i + 1 < n && a[i] > a[i+1]){
+        i++;
+    }
+
+    return i == (n - 1);
 }
 
 int main(){
