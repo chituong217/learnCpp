d850796a25e41d74dca4bd3e45458cac77041fbb
chituong217 <chituong217@gmail.com>
2025-12-10 21:59:30 +0700

    cau 2

---
+++ CSLT_LyThuyet/Mock_Test/Mock6/Cau2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+void multiplyMatrices(int A[][100], int B[][100], int C[][100], int m, int p, int n){
+    for (int i = 0; i < m; i++){
+        for (int j = 0; j < n; j++){
+            C[i][j] = 0;
+            for (int k = 0; k < p; k++){
+                C[i][j] += A[i][k] + B[k][j];
+            }
+        }
+    }
+}
