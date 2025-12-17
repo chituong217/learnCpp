b045f0021e32487087bd181555360e7a042c1c2c
chituong217 <chituong217@gmail.com>
2025-12-17 13:22:02 +0700

    test 15 upd

--- test/test15.cpp
+++ test/test15.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+
+// vi du 1
 int findLES(int a[], int n){
     if (n == 0) return 0;
 
@@ -28,6 +30,74 @@ int findLES(int a[], int n){
     return maxLen;
 }
 
+// vi du 2
+int findLongestIncreaseByOneSubsequent(int a[], int n){
+    if (n == 0) return 0;
+
+    int longestLen = 0;
+    int currentLen = 1;
+
+    for (int i = 1; i < n; i++){
+        if (a[i] - a[i-1] == 1){
+            currentLen++;
+        }
+        else{
+            if (currentLen > longestLen){
+                longestLen = currentLen;
+            }
+
+            currentLen = 1;
+        }
+    }
+
+    if (currentLen > longestLen){
+        longestLen = currentLen;
+    }
+
+    return longestLen;
+}
+
+
+// vidu 3
+int findLongestIncreaseArray(int a[], int n){
+    if (n == 0) return 0;
+    else if (n == 1) return 1;
+
+    int globalDiff = -9999;
+    int longestLen = 1;
+    int currentLen = 1;
+
+    for (int i = 1; i < n; i++){
+        int currentDiff = a[i] - a[i - 1];
+        if (currentDiff > 0){
+            if (currentDiff == globalDiff){
+                currentLen++;
+            }
+            else{
+                if (currentLen > longestLen){
+                    longestLen = currentLen;
+                }
+
+                globalDiff = currentDiff;
+                currentLen = 2;
+            }
+        }
+        else{
+            if (currentLen > longestLen){
+                longestLen = currentLen;
+            }
+            currentLen = 1;
+            globalDiff = -9999;
+        }
+    }
+
+    if (currentLen > longestLen){
+        longestLen = currentLen;
+    }
+
+    return longestLen;
+}
+
 int main(){
     int n;
     cin >> n;
@@ -36,7 +106,7 @@ int main(){
         cin >> a[i];
     }
 
-    cout << findLES(a, n) << endl;
+    cout << findLongestIncreaseArray(a, n) << endl;
 
     return 0;
 }
