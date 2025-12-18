11188e47c0585bf135d14afdc32e05badc973319
chituong217 <chituong217@gmail.com>
2025-12-18 11:38:31 +0700

    cau 3

---
+++ CSLT_LyThuyet/Mock_Test/Mock9/Cau3.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+};
+
+struct List{
+    Node* head;
+};
+
+void removeDuplicates(List &L){
+    if (L.head == NULL){
+        return;
+    }
+    else if (L.head->next == NULL){
+        return;
+    }
+
+    Node dummy;
+    Node* tail = &dummy;
+
+    tail->next = L.head;
+    while (tail != NULL && tail->next != NULL){
+        if (tail->data == tail->next->data){
+            Node* del = tail->next;
+            tail->next = del->next;
+
+            delete del;
+        }
+        else{
+            tail = tail->next;
+        }
+    }
+
+    L.head = dummy.next;
+}
