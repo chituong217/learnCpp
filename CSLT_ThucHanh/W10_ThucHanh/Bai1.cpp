54464d01a5b50435a9c4805d34fe4c113eabe94b
chituong217 <chituong217@gmail.com>
2025-12-19 08:59:49 +0700

    bai1

--- CSLT_ThucHanh/W10_ThucHanh/Bai1.cpp
+++ CSLT_ThucHanh/W10_ThucHanh/Bai1.cpp
@@ -31,3 +31,79 @@ int CompareData(Data info1, Data info2){
         return -1;
     }
 }
+
+// Bai 2
+void InitList(List &l){
+    l.pHead = NULL;
+    l.pTail = NULL;
+}
+
+// Bai 3
+bool isEmptyList(List l){
+    if (l.pHead == NULL){
+        return true;
+    }
+    return false;
+}
+
+// Bai 4
+void PrintList(List l){
+    if (l.pHead == NULL) return;
+
+    Node* tmp = l.pHead;
+    while (tmp != NULL){
+        cout << tmp->info.x << ' ';
+        tmp = tmp->pNext;
+    }
+}
+
+// Bai 5
+Node* CreateNode(Data info){
+    Node* newNode = new Node;
+    if (newNode == NULL){
+        return NULL;
+    }
+
+    newNode->info = info;
+    newNode->pNext = NULL;
+
+    return newNode;
+}
+
+// Bai 6
+Node* GetNodePointer(List l, Data info){
+    if (l.pHead == NULL){
+        return NULL;
+    }
+
+    Node* tmp = l.pHead;
+    while(tmp != NULL){
+        if (tmp->info.x == info.x){
+            return tmp;
+        }
+        tmp = tmp->pNext;
+    }
+
+    return NULL;
+}
+
+// Bai 7
+Node* GetNodePointer(List l, int index){
+    if (l.pHead == NULL || index < 0){
+        return NULL;
+    }
+
+    int listIndex = 0;
+    Node* tmp = l.pHead;
+    
+    while (tmp != NULL){
+        if (listIndex == index){
+            return tmp;
+        }
+
+        tmp = tmp->pNext;
+        listIndex++;
+    }
+
+    return NULL;
+}
