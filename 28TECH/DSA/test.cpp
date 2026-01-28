dd5e4f5e241ddac75c3b84d794193e47e5e27b5d
chituong217 <chituong217@gmail.com>
2026-01-29 00:13:17 +0700

    upd

--- 28TECH/DSA/test.cpp
+++ 28TECH/DSA/test.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main(){
     int n;
     cin >> n;
-    pair<int, int> p[n];
+    pair<int, int> p[n]; // mang cac pair
     for (int i = 0; i < n; i++){
         cin >> p[i].first >> p[i].second;
     }
