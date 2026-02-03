064a64ce14ed52bebb73987fd110b610464d0d5c
chituong217 <chituong217@gmail.com>
2026-02-04 00:07:14 +0700

    upd

--- 28TECH/DSA/test.cpp
+++ 28TECH/DSA/test.cpp
@@ -2,19 +2,33 @@
 #include <vector>
 #include <cmath>
 #include <cstdlib>
+#include <set>
+#include <map>
+#include <string>
+
 
 using namespace std;
 
 int main(){
-    int n;
-    cin >> n;
-    pair<int, int> p[n]; // mang cac pair
-    for (int i = 0; i < n; i++){
-        cin >> p[i].first >> p[i].second;
-    }
+    map<int, int> mp;
+    mp.insert({1, 2}); // insert vao cap {1, 2}
+    mp.insert({2, 3});
+    mp.insert({3, 4});
+    mp.insert({5, 2});
+    mp.insert({1, 5}); // do trong map da co key 1 roi nen se khong insert nua
 
-    for (int i = 0; i < n; i++){
-        cout << p[i].first << ' ' << p[i].second << endl;
+    cout << mp.size() << endl; // 4
+
+    mp[1] = 3; // key 1 bay gio co value la 3
+    mp[4] = 2; // them cap {4, 2}
+
+    cout << mp.size() << endl; // 5
+    cout << mp[5] << endl; // value cua key 5
+
+    for (map<int, int>::iterator it = mp.begin(); it != mp.end(); it++){
+        // cout << (*it).first << ' ' << (*it).second << endl;
+        cout << it->first << ' ' << it->second << endl;
     }
+    
     return 0;
 }
